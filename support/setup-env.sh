fd9f9e23ebb3e028013c55c8219642430073d221
Evan Clements <evan.clements.926@gmail.com>
2023-07-24 09:45:26 -0400

    Update setup-env.sh

--- support/setup-env.sh
+++ support/setup-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # export TOOLCHAIN_VERSION?
-export PATH="/opt/mips32-mti-elf/${TC_VERSION}/bin/mips-mti-elf-"
-# export CROSS_COMPILE=/opt/trimui-toolchain/usr/bin/arm-buildroot-linux-gnueabi-
+export PATH=/opt/mips-mti-elf/${TC_VERSION}/bin/mips-mti-elf-
+export CROSS_COMPILE=/opt/mips-mti-elf/${TC_VERSION}/bin/mips-mti-elf-
 # export PREFIX=/opt/trimui-toolchain/usr/arm-buildroot-linux-gnueabi/sysroot/usr
