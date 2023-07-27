b7522d84edb41999fb52d7d95bc9e97ce1a9dbdb
Evan Clements <evan.clements.926@gmail.com>
2023-07-27 09:55:34 -0400

    added function to .bashrc to assist in updating SDK to work with new toolchain

--- support/setup-env.sh
+++ support/setup-env.sh
@@ -3,4 +3,4 @@
 # export TOOLCHAIN_VERSION?
 export PATH=/opt/sf2000-toolchain/bin/mips-mti-elf-:/bin:/usr/bin
 export CROSS_COMPILE=/opt/sf2000-toolchain/bin/mips-mti-elf-
-# alias update-toolchain="grep -rli "mips32-mti-elf\/2019.09-03-2" * | xargs -i@ sed -i "" 's/mips32-mti-elf\/2019.09-03-2/sf2000-toolchain/g' @"
+export update-toolchain="grep -rli "mips32-mti-elf\/2019.09-03-2" * | xargs -i@ sed -i "" 's/mips32-mti-elf\/2019.09-03-2/sf2000-toolchain/g' @"
