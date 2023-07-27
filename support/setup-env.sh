7267ec8b278a7c58478d51d1b4e9b5e7a9d91d65
Evan Clements <evan.clements.926@gmail.com>
2023-07-27 10:34:21 -0400

    Updated setup-env.sh to include command for updating standard SDK

--- support/setup-env.sh
+++ support/setup-env.sh
@@ -3,4 +3,7 @@
 # export TOOLCHAIN_VERSION?
 export PATH=/opt/sf2000-toolchain/bin/mips-mti-elf-:/bin:/usr/bin
 export CROSS_COMPILE=/opt/sf2000-toolchain/bin/mips-mti-elf-
-export update-toolchain="grep -rli "mips32-mti-elf\/2019.09-03-2" * | xargs -i@ sed -i "" 's/mips32-mti-elf\/2019.09-03-2/sf2000-toolchain/g' @"
+echo "Use the following command to update the official hcrtos to work with this toolchain:"
+echo ""
+echo "grep -rli \"mips32-mti-elf\/2019.09-03-2\" * | xargs -i@ sed -i 's/mips32-mti-elf\/2019.09-03-2/sf2000-toolchain/g' @"
+echo ""
